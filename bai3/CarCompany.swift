c1783af3da07ff3cd7fe18b7c037914fb57971bc
Nguyễn Ngọc Sơn <nguyenngocson@MacBook-Pro-cua-Nguyen.local>
2023-07-06 20:33:15 +0700

    init project

---
+++ bai3/CarCompany.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum CarCompany: String {
+    case MAI_LINH = "Mai Linh"
+    case VINA_SUN = "Vinasun"
+    case VINA_TAXI = "Vinataxi"
+    case GROUP = "Group"
+    case G7 = "G7"
+}
---
+++ bai3/Service.swift
@@ -0,0 +1,9 @@
+import Foundation
+enum Service: String {
+    case XE_4_LON = "xe 4 chỗ lớn"
+    case XE_4_NHO = "xe 4 chỗ nhỏ"
+    case XE_7 = "xe 7 chỗ"
+    case XE_VIOS = "xe vios"
+    case XE_INOVA = "xe inova"
+    case XE_ECO = "xe eco"
+}
